7a0437bfe06b38154b12f59abb46401b3d9682a8
Mark Sagal <admin@pinoycentral.net>
2017-02-26 02:04:30 +0800

    Fix: CoreController - namespace autoloader

--- application/core/CoreController.php
+++ application/core/CoreController.php
@@ -5,25 +5,10 @@ abstract class CoreController extends CI_Controller
     {
         parent::__construct();
         spl_autoload_extensions('.php');
-        spl_autoload_register(function ($sNameSpace) {
-            $aNameSpace = explode('\\', $sNameSpace);
-            $iNameSpace = count($aNameSpace);
-            $sClass = $aNameSpace[$iNameSpace - 1];
-            $aClass = preg_split('/(?=[A-Z])/', $sClass);
-            $iLogics = count($aClass);
-            $iCount = 0;
-            $sLogic = '';
+        spl_autoload_register(function ($sClass) {
+            $sLogicPath = APPPATH . 'logic' . DS . $sClass . '.php';
 
-            if ($iLogics > 1) {
-                foreach ($aClass as $sClass) {
-                    $iCount++;
-                    $sLogic .= $iLogics === $iCount ? $sClass . '.php' : $sClass;
-                }
-            }
-
-            $sLogicPath = APPPATH . 'logic' . DS . $sLogic;
-
-            if (file_exists($sLogicPath) === true) {
+            if (file_exists($sLogicPath) === true && empty($sClass) !== true) {
                 require_once $sLogicPath;
             }
         });
